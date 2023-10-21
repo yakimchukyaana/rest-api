b8d38164bdbed54d7f1770a095d434f9f37894d5
Яна Якимчук <jerrrytyson@Anas-MacBook-Air.local>
2023-10-21 17:08:49 +0800

    add collection tests

---
+++ src/test/java/com/demoqa/tests/CollectionTests.java
@@ -0,0 +1,216 @@
+package com.demoqa.tests;
+
+import com.codeborne.selenide.Condition;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Cookie;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
+import static com.demoqa.tests.TestData.login;
+import static com.demoqa.tests.TestData.password;
+import static io.restassured.RestAssured.given;
+import static io.restassured.http.ContentType.JSON;
+import static java.lang.String.format;
+import static org.hamcrest.Matchers.is;
+
+public class CollectionTests extends TestBase {
+
+    @Test
+    void addBookToCollectionTest() {
+
+        //String authData = "{\"userName\":\"test123456\", \"password\":\"Test123456@\"}"; //BAD PRACTICE
+        String authData = "{\"userName\":\"testtestov31\",\"password\":\"Testtestov31_%\"}"; //BAD PRACTICE
+
+        Response authResponse = given()
+                .log().uri()
+                .log().method()
+                .log().body()
+                .contentType(JSON)
+                .body(authData)
+                .when()
+                .post("/Account/v1/Login")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .extract().response();
+
+        given()
+                .log().uri()
+                .log().method()
+                .log().body()
+                .contentType(JSON)
+                .header("Authorization", "Bearer " + authResponse.path("token"))
+                .queryParam("UserId" + authResponse.path("userId"))
+                .when()
+                .delete("/BookStore/v1/Books")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(204);
+
+        String isbn = "9781449365035";
+        String bookData = format("{\"userId\":\"%s\", \"collectionOfIsbns\":[{\"isbn\":\"%s\"}]}",
+                authResponse.path("userId"), isbn);
+
+        given()
+                .log().uri()
+                .log().method()
+                .log().body()
+                .contentType(JSON)
+                .header("Authorization", "Bearer " + authResponse.path("token"))
+                .body(bookData)
+                .when()
+                .post("/BookStore/v1/Books")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(201);
+
+        open("/favicon.ico");
+        getWebDriver().manage().addCookie(new Cookie("userID", authResponse.path("userId")));
+        getWebDriver().manage().addCookie(new Cookie("expires", authResponse.path("expires")));
+        getWebDriver().manage().addCookie(new Cookie("token", authResponse.path("token")));
+
+        open("/profile");
+        $(".ReactTable").shouldHave(text("Speaking JavaScript"));
+    }
+
+    @Test
+    void addBookToCollection_withDelete1Book_Test() {
+        String authData = "{\"userName\":\"testtestov31\",\"password\":\"Testtestov31_%\"}";
+
+        Response authResponse = given()
+                .log().uri()
+                .log().method()
+                .log().body()
+                .contentType(JSON)
+                .body(authData)
+                .when()
+                .post("/Account/v1/Login")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .extract().response();
+
+        String isbn = "9781449365035";
+        String deleteBookData = format("{\"userId\":\"%s\",\"isbn\":\"%s\"}",
+                authResponse.path("userId") , isbn);
+
+        given()
+                .log().uri()
+                .log().method()
+                .log().body()
+                .contentType(JSON)
+                .header("Authorization", "Bearer " + authResponse.path("token"))
+                .body(deleteBookData)
+                .when()
+                .delete("/BookStore/v1/Book")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(204);
+
+        String bookData = format("{\"userId\":\"%s\",\"collectionOfIsbns\":[{\"isbn\":\"%s\"}]}",
+                authResponse.path("userId") , isbn);
+
+        given()
+                .log().uri()
+                .log().method()
+                .log().body()
+                .contentType(JSON)
+                .header("Authorization", "Bearer " + authResponse.path("token"))
+                .body(bookData)
+                .when()
+                .post("/BookStore/v1/Books")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(201);
+
+        open("/favicon.ico");
+        getWebDriver().manage().addCookie(new Cookie("userID", authResponse.path("userId")));
+        getWebDriver().manage().addCookie(new Cookie("expires", authResponse.path("expires")));
+        getWebDriver().manage().addCookie(new Cookie("token", authResponse.path("token")));
+
+        open("/profile");
+        $(".ReactTable").shouldHave(text("Speaking JavaScript"));
+    }
+
+    @Test
+    void negative400BookToCollectionTest() {
+
+        String authData = "{\"userName\":\"test123456\", \"password\":\"Test123456@\"}"; //BAD PRACTICE
+
+        Response authResponse = given()
+                .log().uri()
+                .log().method()
+                .log().body()
+                .contentType(JSON)
+                .body(authData)
+                .when()
+                .post("/Account/v1/Login")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .extract().response();
+
+
+        String isbn = "9781449325862";
+        String bookData = format("{\"userId\":\"%s\", \"collectionOfIsbns\":[{\"isbn\":\"%s\"}]}",
+                authResponse.path("userId"), isbn);
+
+        given()
+                .log().uri()
+                .log().method()
+                .log().body()
+                .contentType(JSON)
+                .header("Authorization", "Bearer " + authResponse.path("token"))
+                .body(bookData)
+                .when()
+                .post("/BookStore/v1/Books")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(400)
+                .body("code", is("1210"))
+                .body("message", is("ISBN already present in the User's Collection!"));
+
+        open("/favicon.ico");
+        getWebDriver().manage().addCookie(new Cookie("userID", authResponse.path("userId")));
+        getWebDriver().manage().addCookie(new Cookie("expires", authResponse.path("expires")));
+        getWebDriver().manage().addCookie(new Cookie("token", authResponse.path("token")));
+
+        open("/profile");
+        $("#userName-value").shouldHave(text(login));
+    }
+
+    @Test
+    void negative401AddBookToCollectionTest() {
+
+        String userId = "862cbbea-c3e8-4a39-be64-3b053aedeef2";
+        String isbn = "9781449325862";
+        String bookData = format("{\"userId\":\"%s\", \"collectionOfIsbns\":[{\"isbn\":\"%s\"}]}",
+                userId, isbn);
+
+        given()
+                .log().uri()
+                .log().method()
+                .log().body()
+                .contentType(JSON)
+                .body(bookData)
+                .when()
+                .post("/BookStore/v1/Books")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(401)
+                .body("code", is("1200"))
+                .body("message", is("User not authorized!"));
+    }
+}
